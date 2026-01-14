ccea19da8b10231f528bb4229adb890d7c7b3a85
Rasmus lind <rasmus1234555@hotmail.dk>
2026-01-14 21:33:11 +0100

    fix zoom

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -5,7 +5,7 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 export const Input = forwardRef<HTMLInputElement, InputProps>(
   ({ className = "", ...props }, ref) => {
     const baseStyles =
-      "w-full px-3 py-2 rounded-lg bg-stone-700 border border-stone-600 text-stone-100 font-ui text-sm placeholder:text-stone-500 focus:outline-none focus:ring-2 focus:ring-stone-400/50";
+      "w-full px-3 py-2 rounded-lg bg-stone-700 border border-stone-600 text-stone-100 font-ui text-base placeholder:text-stone-500 focus:outline-none focus:ring-2 focus:ring-stone-400/50";
 
     return (
       <input
