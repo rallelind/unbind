a54b636ed58d25f5d96ae2425b115c8bd0edb0e2
rallelind <rasmus.sommerlund.lind@gmail.com>
2026-02-02 21:36:16 +0100

    buggo fix

--- mobile/Unbind/Unbind/Config.swift
+++ mobile/Unbind/Unbind/Config.swift
@@ -2,7 +2,7 @@ import Foundation
 
 enum Config {
     static let apiBaseUrl: String = {
-        #if DEBUG
+        #if targetEnvironment(simulator)
         return "http://localhost:3000/api"
         #else
         return "https://unbind.up.railway.app/api"
